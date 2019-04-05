2602565cf0df16f747a1384227ec43302409282a
Anna Szmyga <anna.szmyga@o2.pl>
2019-04-05 16:32:25 +0200

    Add responsive styles for main menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+// Menu for mobiles
+
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function (event) {
+  event.preventDefault();
+  document.querySelector('.menu').classList.toggle('show');
+});
