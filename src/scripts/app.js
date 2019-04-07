8bb51f19c170db80d87a7b6ed556df3ddf81d0c7
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-04-07 23:50:20 +0200

    Fix script syntax

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,5 +6,16 @@ outlinesButtons.forEach(function (button) {
   button.addEventListener('click', function (event) {
     event.preventDefault();
     button.classList.toggle('active');
+let stars = document.querySelectorAll('.stars');
+
+stars.forEach(function (singleRating) {
+  singleRating.addEventListener('click', function (event) {
+    if (event.target.tagName.toLowerCase() !== 'span') return;
+
+    if (event.target.classList.contains('full')) {
+      event.target.classList.remove('full');
+    } else {
+      event.target.classList.add('full');
+    }
   });
 });
