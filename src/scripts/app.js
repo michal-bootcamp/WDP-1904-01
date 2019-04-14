f83c4a4fd6c5a47fe4ecc42d687f70b6bc9d581f
ypablo <yfedora@gmail.com>
2019-04-15 00:21:13 +0100

    WDP190401-18 Modify carousel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,42 @@
 console.log('hello');
+/*
+var touchSensitivity = 5;
+$('.carousel').on('touchstart', function(event) {
+  var xClick = event.originalEvent.touches[0].pageX;
+  $(this).one('touchmove', function(event) {
+    var xMove = event.originalEvent.touches[0].pageX;
+    if (Math.floor(xClick - xMove) > touchSensitivity) {
+      $(this).carousel('next');
+    } else if (Math.floor(xClick - xMove) < -touchSensitivity) {
+      $(this).carousel('prev');
+    }
+  });
+  $('.carousel').on('touchend', function() {
+    $(this).off('touchmove');
+  });
+});
+*/
+/*
+$('.carousel[data-type="multi"] .item').each(function() {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next
+    .children(':first-child')
+    .clone()
+    .appendTo($(this));
+
+  for (var i = 0; i < 2; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+
+    next
+      .children(':first-child')
+      .clone()
+      .appendTo($(this));
+  }
+});
+*/
