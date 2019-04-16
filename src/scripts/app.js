02c59a5a007cc0c504d5ca1c5531181398b1f21b
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-04-16 22:22:36 +0200

    Change script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,11 +6,34 @@ outlinesButtons.forEach(function (button) {
   button.addEventListener('click', function (event) {
     event.preventDefault();
     button.classList.toggle('active');
+  });
+});
+
+// WDP190401-19
 let stars = document.querySelectorAll('.stars');
+
+var getSiblings = function (elem) {
+  var siblings = [];
+  var sibling = elem.parentNode.firstChild;
+
+  while (sibling) {
+    if (sibling.nodeType === 1 && sibling !== elem) {
+      siblings.push(sibling);
+    }
+    sibling = sibling.nextSibling;
+  }
+
+  return siblings;
+};
 
 stars.forEach(function (singleRating) {
   singleRating.addEventListener('click', function (event) {
-    if (event.target.tagName.toLowerCase() !== 'span') return;
+    let elem = event.target;
+    let siblings = getSiblings(elem);
+
+    for (var i = 0; i < siblings.length; i++) {
+      siblings[i].classList.remove('full');
+    }
 
     if (event.target.classList.contains('full')) {
       event.target.classList.remove('full');
