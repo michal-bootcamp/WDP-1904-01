66b632349f94f9c09776ddcee07eb148e6f4de6f
Anna Szmyga <anna.szmyga@o2.pl>
2019-04-16 16:00:42 +0200

    Add gallery slider js code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
+
 console.log('hello');
 
 const outlinesButtons = document.querySelectorAll('.btn-outline');
@@ -12,3 +14,44 @@ outlinesButtons.forEach(function (button) {
 /*
 $("[data-toggle='tooltip']").tooltip();
 */
+
+// gallery sliders
+
+const galleryBoxes = document.querySelectorAll('.tab-pane');
+
+galleryBoxes.forEach(function (box) {
+  const slider = tns({
+    container: box.querySelector('.gallery-slider'),
+    items: 6,
+    gutter: 10,
+    slideBy: 2,
+    controlsText: ['', ''],
+    nav: false
+  });
+
+  const info = slider.getInfo();
+
+  for (let i = 0; i < info.slideItems.length; i++) {
+    info.slideItems[i].addEventListener('click', function (e) {
+      for (let n = 0; n < info.slideItems.length; n++) {
+        info.slideItems[n].querySelector('.slider-image').classList.remove('active');
+      }
+      info.slideItems[i].querySelector('.slider-image').classList.add('active');
+      const activeImage = box.querySelector('.slider-image.active').style
+        .backgroundImage;
+      box.querySelector('.image').style.backgroundImage = activeImage;
+    });
+  }
+
+  box
+    .querySelector("[data-controls='prev']")
+    .classList.add('prev', 'btn-main', 'no-hover');
+  box.querySelector("[data-controls='prev']").innerHTML =
+    '<i class="fas fa-chevron-left">';
+
+  box
+    .querySelector("[data-controls='next']")
+    .classList.add('next', 'btn-main', 'no-hover');
+  box.querySelector("[data-controls='next']").innerHTML =
+    '<i class="fas fa-chevron-right">';
+});
