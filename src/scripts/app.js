9db80ce20e12648bdc7024f5f0ec8575536a14ca
Anna Szmyga <anna.szmyga@o2.pl>
2019-04-25 12:15:45 +0200

    Add fade animation for gallery

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -18,13 +18,14 @@ $("[data-toggle='tooltip']").tooltip();
 // gallery sliders
 
 const galleryBoxes = document.querySelectorAll('.tab-pane');
+const fadeLayer = document.querySelector('.fade-layer');
 
 galleryBoxes.forEach(function (box) {
   const slider = tns({
     container: box.querySelector('.gallery-slider'),
     items: 6,
     gutter: 10,
-    slideBy: 2,
+    slideBy: 3,
     controlsText: ['', ''],
     nav: false
   });
@@ -39,19 +40,28 @@ galleryBoxes.forEach(function (box) {
       info.slideItems[i].querySelector('.slider-image').classList.add('active');
       const activeImage = box.querySelector('.slider-image.active').style
         .backgroundImage;
+
       box.querySelector('.image').style.backgroundImage = activeImage;
+
+      // set fade animation
+      fadeLayer.style.display = 'block';
+
+      setTimeout(function () {
+        fadeLayer.style.backgroundColor = 'transparent';
+      }, 60);
+
+      setTimeout(function () {
+        fadeLayer.style.backgroundColor = '#fff';
+        fadeLayer.style.display = 'none';
+      }, 200);
     });
   }
 
-  box
-    .querySelector("[data-controls='prev']")
-    .classList.add('prev', 'btn-main', 'no-hover');
-  box.querySelector("[data-controls='prev']").innerHTML =
-    '<i class="fas fa-chevron-left">';
+  const buttonPrev = box.querySelector("[data-controls='prev']");
+  const buttonNext = box.querySelector("[data-controls='next']");
 
-  box
-    .querySelector("[data-controls='next']")
-    .classList.add('next', 'btn-main', 'no-hover');
-  box.querySelector("[data-controls='next']").innerHTML =
-    '<i class="fas fa-chevron-right">';
+  buttonPrev.classList.add('prev', 'btn-main', 'no-hover');
+  buttonPrev.innerHTML = '<i class="fas fa-chevron-left">';
+  buttonNext.classList.add('next', 'btn-main', 'no-hover');
+  buttonNext.innerHTML = '<i class="fas fa-chevron-right">';
 });
